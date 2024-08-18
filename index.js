82160935036abeea7f0d5d12c1387b63e72e71a6
manjeetsharma0796 <manjeetsharma0796@gmail.com>
2024-08-19 00:16:53 +0530

    server add

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+
+const __root = path.resolve()
+
+app.get('/', (req,res)=> {
+    res.sendFile(`${__root}/public/home.html`)
+})
+
+app.use(express.static(`${__root}/public`))
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log("http://localhost:8000");
+    
+})
