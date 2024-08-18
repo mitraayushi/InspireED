fe56b9a67b7d3a9111bcac83bdb11fd6c627d271
Ayushi Mitra <ayushimitra22@gmail.com>
2024-08-18 22:42:36 +0530

    website

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function openInvestPopup(projectTitle) {
+
+    document.getElementById('invest-popup').style.display = 'flex';
+    
+    document.getElementById('project-title').innerText = 'Invest in ' + projectTitle;
+}
+
+function closeInvestPopup() {
+    
+    document.getElementById('invest-popup').style.display = 'none';
+}
+
+function sendInvestment() {
+    
+    const amount = document.getElementById('invest-amount').value;
+    if (amount) {
+        alert(`Investment of ${amount} sent successfully!`);
+       
+        closeInvestPopup();
+    } else {
+        alert('Please enter a valid amount.');
+    }
+}
